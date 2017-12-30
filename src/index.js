56405f0dc40747bf5d3b2869955f8e9f2afcc171
picosix <picosix.com@gmail.com>
2017-12-30 17:28:58 +0700

    Update docker file to make sure nginx is started when container was boot

--- src/index.js
+++ src/index.js
@@ -7,36 +7,12 @@ const _ = require('lodash');
 const slug = require('slug');
 const sharp = require('sharp');
 const shelljs = require('shelljs');
-const winston = require('winston');
 
 const config = require('./config');
 const { createFolder } = require('./utils');
+const logger = require('./logger');
 
 const app = express();
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/warn.log', level: 'warn' }),
-    new winston.transports.File({ filename: 'logs/combined.log' })
-  ],
-  exceptionHandlers: [
-    new winston.transports.File({ filename: 'logs/exceptions.log' })
-  ]
-});
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple()
-    })
-  );
-}
-
 // Midleware
 app.use(bodyParser.json());
 app.use(methodOverride());

d409e0f85f141cb8bd9390c4525e84cafa43b734
Tuan Nguyen <picosix.com@gmail.com>
2018-02-28 23:39:42 +0700

    update statistic

--- api/src/routes/statictis.js
+++ api/src/routes/statictis.js
@@ -9,32 +9,27 @@ const findDriveDetail = util.promisify(nd.drivesDetail);
 
 module.exports = async (req, res, next) => {
   try {
+    let storage = {
+      total: 0,
+      used: 0,
+      available: 0,
+      usedPercent: 0,
+      availablePercent: 0
+    };
     const mainDisk = (await findDriveDetail(await findDrives())).filter(
       drive => drive && drive.drive === "/dev/sda1"
     );
-    if (!mainDisk.length) {
-      return res.json({
-        images: 0,
-        cache: 0,
-        storage: {
-          total: 0,
-          used: 0,
-          available: 0,
-          usedPercent: 0,
-          availablePercent: 0
-        }
-      });
+    if (mainDisk.length) {
+      const {
+        total,
+        used,
+        available,
+        usedPer: usedPercent,
+        freePer: availablePercent
+      } = mainDisk[0];
+      storage = { total, used, available, usedPercent, availablePercent };
     }
 
-    const {
-      total,
-      used,
-      available,
-      usedPer: usedPercent,
-      freePer: availablePercent
-    } = mainDisk[0];
-
-    const storage = { total, used, available, usedPercent, availablePercent };
     const images = await db.model("Image").count();
     const cache = await db.model("Cache").count();
 
